1f1a62489025fa0678d545c9959e755024a172d3
Erik Voigt <100310971+VoErik@users.noreply.github.com>
2024-11-07 23:14:12 +0100

    initial commit

--- domgen/augment/__init__.py
+++ domgen/augment/__init__.py
@@ -1,5 +1,4 @@
 from domgen.augment._sample import SampleAugmentation # for every object belonging to the public api
-from domgen.augment._foo._bar import Bar
 
 
-__all__ = ['SampleAugmentation', 'Bar']
+__all__ = ['SampleAugmentation']
