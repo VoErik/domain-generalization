435bd3bd91105d899fe1994d58bfd75855b84d6a
Erik Voigt <erik.voigt@student.uni-bamberg.de>
2024-11-07 22:50:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Installation
+From `root` run:
+```console
+python -m pip install -e .
+```
---
+++ domgen/augment/__init__.py
@@ -0,0 +1,5 @@
+from domgen.augment._sample import SampleAugmentation # for every object belonging to the public api
+from domgen.augment._foo._bar import Bar
+
+
+__all__ = ['SampleAugmentation', 'Bar']
