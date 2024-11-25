9d8629ecfde7850d99d286c5fed0ad5814197cb4
Erik Voigt <erik.voigt@student.uni-bamberg.de>
2024-11-25 22:31:39 +0100

    add Camelyon17 Dataset class to the _datasets.py

--- domgen/data/_datasets.py
+++ domgen/data/_datasets.py
@@ -1,15 +1,18 @@
 import os
 from collections import defaultdict
+import random
 
 import torch
-from torch.utils.data import DataLoader, ConcatDataset
+from torch.utils.data import DataLoader, ConcatDataset, Subset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import transforms
+from typing import Any
 
 """To add a new dataset, just create a class that inherits from `DomainDataset`."""
 
 DOMAIN_NAMES = {
     'PACS': ["art_painting", "cartoon", "photo", "sketch"],
+    'camelyon17': ["center_0", "center_1", "center_2", "center_3", "center_4"],
 }
 
 
@@ -25,18 +28,25 @@ class MultiDomainDataset:
 
 
 class DomainDataset(MultiDomainDataset):
-    def __init__(self, root, test_domain, augment):
+    def __init__(
+            self,
+            root: str,
+            test_domain: int,
+            augment: Any,
+            subset: float = None,
+    ):
         super().__init__()
         self.domains = sorted([directory.name for directory in os.scandir(root) if directory.is_dir()])
         self.test_domain = test_domain
-        # resize to 224x224 and normalize (ImageNet)
+        self.subset = subset
+        # base augment = ImageNet
+        input_size = self.input_shape[-2], self.input_shape[-1]
         transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize(input_size),
             transforms.ToTensor(),
             # transforms.Normalize(
             # mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-
         self.data = []
         for i, domain in enumerate(self.domains):
             if augment and (i != self.test_domain):
@@ -47,9 +57,20 @@ class DomainDataset(MultiDomainDataset):
             path = os.path.join(root, domain)
             domain_dataset = ImageFolder(path, transform=domain_transform)
 
-            self.data.append(domain_dataset)
+            if self.subset is not None:
+                num_samples = int(len(domain_dataset) * self.subset)
+                indices = list(range(len(domain_dataset)))
+                random.shuffle(indices)
+                subset_indices = indices[:num_samples]
 
-        self.input_shape = (3, 224, 224,)
+                samples = [domain_dataset.samples[i] for i in subset_indices]
+                targets = [domain_dataset.targets[i] for i in subset_indices]
+                domain_dataset.samples = samples
+                domain_dataset.targets = targets
+
+            self.data.append(domain_dataset)
+        for i, domain_data in enumerate(self.data):
+            print(f"Domain {i}: {len(domain_data)} samples")
         self.num_classes = len(self.data[-1].classes)
         self.classes = list(self.data[-1].classes)
         self.idx_to_class = dict(zip(range(self.num_classes), self.classes))
@@ -64,9 +85,11 @@ class DomainDataset(MultiDomainDataset):
                 size_dict[domain_name_map[i]] = len(domain_dataset.imgs)
         return size_dict
 
-    def generate_loaders(self,
-                         batch_size: int = 32,
-                         partition_size: float = 0.8) -> (DataLoader, DataLoader, DataLoader):
+    def generate_loaders(
+            self,
+            batch_size: int = 32,
+            partition_size: float = 0.8,
+    ) -> (DataLoader, DataLoader, DataLoader):
         """
         Generates DataLoaders for training and testing domains.
         Parameters
@@ -101,3 +124,12 @@ class PACS(DomainDataset):
     def __init__(self, root, test_domain):
         self.dir = os.path.join(root, "PACS/")
         super().__init__(self.dir, test_domain, augment=None)
+
+
+class Camelyon17(DomainDataset):
+    domains = DOMAIN_NAMES['camelyon17']
+    input_shape = (3, 96, 96)
+
+    def __init__(self, root, test_domain):
+        self.dir = os.path.join(root, "camelyon17/")
+        super().__init__(self.dir, test_domain, augment=None, subset=0.2)
