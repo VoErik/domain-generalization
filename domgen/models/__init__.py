a07a2df9ae1e59f851a956c6045301d87cb524b0
Erik Voigt <erik.voigt@student.uni-bamberg.de>
2024-12-02 22:25:30 +0100

    update __init__.py
    exposes more functions

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -1,3 +1,12 @@
-from domgen.models._model_config import get_model
+from domgen.models._training import train_model, train_epoch, validate, test
+from domgen.models._model_config import get_model, get_criterion, get_optimizer, get_device
 
-__all__ = ['get_model']
+__all__ = ['get_model',
+           'train_model',
+           'train_epoch',
+           'validate',
+           'test',
+           'get_model',
+           'get_criterion',
+           'get_optimizer',
+           'get_device']
