3c9d3496196f40ef77a1703cb46d929786365254
Hilke Ahlers <hilke-maria.ahlers@stud.uni-bamberg.de>
2024-12-03 13:57:41 +0100

    - applied requested changes to the files, all needed implementation now condensed in one file - updated _init_.py and renamed file for DenseNet functionality (_densenet.py)

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -1,3 +1,8 @@
 from domgen.models._model_config import get_model
+from domgen.models._densenet import densenet121, densenet169, densenet201, densenet264
 
-__all__ = ['get_model']
+__all__ = ['get_model',
+           'densenet121',
+           'densenet169',
+           'densenet201',
+           'densenet264']
