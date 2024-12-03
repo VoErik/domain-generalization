1ae753ae6aae3ab8bb2def72371b1e2dafd2e515
Hilke Ahlers <hilke-maria.ahlers@stud.uni-bamberg.de>
2024-12-03 13:08:23 +0100

    - applied requested changes to the files, all needed implementation now condensed in one file - updated _init_.py and renamed file for ResNet functionality (_resnet.py)

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -1,3 +1,4 @@
 from domgen.models._model_config import get_model
+from domgen.models._resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 
-__all__ = ['get_model']
+__all__ = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
