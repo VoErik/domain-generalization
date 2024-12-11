496432966fb85cc6f3e2218e15975ebd2b4d5680
erik-workstation <voigterik@outlook.de>
2024-12-11 11:21:47 +0100

    add training script

---
+++ training_full_sweep.ps1
@@ -0,0 +1,19 @@
+$CONFIG_DIR = "config\$args"
+$SCRIPT = "train.py"
+
+if (-Not (Test-Path -Path $CONFIG_DIR)) {
+    Write-Host "Directory $CONFIG_DIR does not exist!"
+    exit
+}
+
+$yamlFiles = Get-ChildItem -Path $CONFIG_DIR -Filter "*.yaml"
+if ($yamlFiles.Count -eq 0) {
+    Write-Host "No YAML files found in $CONFIG_DIR!"
+    exit
+}
+
+$yamlFiles | ForEach-Object {
+    $config = $_.FullName
+    Write-Host "Running $SCRIPT with --config $config"
+    python $SCRIPT --config $config
+}
