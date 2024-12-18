02691203462c77fcc19400db78e75bbac0cc5e41
erik-workstation <voigterik@outlook.de>
2024-12-19 00:51:35 +0100

    add delete model dirs function

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -2,7 +2,7 @@ from domgen.models._resnet import resnet18, resnet34, resnet50, resnet101, resne
 from domgen.models._training import train_model, train_epoch, validate, test
 from domgen.models._model_config import get_model, get_criterion, get_optimizer, get_device
 from domgen.models._densenet import densenet121, densenet169, densenet201, densenet264
-from domgen.models._utils import EarlyStopping
+from domgen.models._utils import EarlyStopping, delete_model_dirs
 from domgen.models._tuning import ParamTuner
 
 __all__ = ['resnet18',
@@ -26,4 +26,5 @@ __all__ = ['resnet18',
            'densenet264',
            'EarlyStopping',
            'ParamTuner',
+           'delete_model_dirs'
            ]
