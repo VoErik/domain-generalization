0e2041ef5334d24f94c0204d1c1fd4d9f01a0f8a
Hilke Ahlers <hilke-maria.ahlers@stud.uni-bamberg.de>
2024-12-31 18:10:23 +0100

    hopefully resolved merge conflict in domgen/models/__init__.py

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -2,8 +2,9 @@ from domgen.models._resnet import _resnet
 from domgen.models._training import train_model, train_epoch, validate, test
 from domgen.models._model_config import get_model, get_criterion, get_optimizer, get_device
 from domgen.models._densenet import _densenet
-from domgen.models._utils import EarlyStopping
+from domgen.models._utils import EarlyStopping, delete_model_dirs
 from domgen.models._tuning import ParamTuner
+
 
 __all__ = ['_resnet',
            'get_model',
