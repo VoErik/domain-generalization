3edf75d79b998032f7b3bb9e5335887afe580ff2
erik-workstation <voigterik@outlook.de>
2025-01-10 12:45:00 +0100

    expose TuningDataset creation function

--- domgen/tuning/__init__.py
+++ domgen/tuning/__init__.py
@@ -1,4 +1,5 @@
 from domgen.tuning._hyperparameter_tuning import ParamTuner
 from domgen.tuning._augmentation_tuner import AugmentationTuner
+from domgen.tuning._tuning_data import create_datasets
 
-__all__ = ['ParamTuner', 'AugmentationTuner']
+__all__ = ['ParamTuner', 'AugmentationTuner', 'create_datasets']
