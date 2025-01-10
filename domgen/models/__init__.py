77cf39a44293b3f31fcf39f46bbf06911bacd19c
erik-workstation <voigterik@outlook.de>
2025-01-10 12:45:34 +0100

    move densenet import to the top to avoid circularity + remove duplicates

--- domgen/models/__init__.py
+++ domgen/models/__init__.py
@@ -1,7 +1,7 @@
 from domgen.models._resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from domgen.models._densenet import densenet121, densenet169, densenet201, densenet264
 from domgen.models._training import DomGenTrainer
 from domgen.models._model_config import get_model, get_criterion, get_optimizer, get_device
-from domgen.models._densenet import densenet121, densenet169, densenet201, densenet264
 from domgen.models._utils import EarlyStopping, delete_model_dirs
 
 __all__ = ['resnet18',
@@ -11,11 +11,9 @@ __all__ = ['resnet18',
            'resnet152',
            'get_model',
            'DomGenTrainer',
-           'get_model',
            'get_criterion',
            'get_optimizer',
            'get_device',
-           'get_model',
            'densenet121',
            'densenet169',
            'densenet201',
