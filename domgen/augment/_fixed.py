b3b0f567a9029400ef510a66847f85f2e942bda5
Erik Voigt <erik.voigt@student.uni-bamberg.de>
2025-01-14 15:23:44 +0100

    add template for FIXED algorithm

---
+++ domgen/augment/_fixed.py
@@ -0,0 +1,2 @@
+# TODO: implement fixed algorithm as proposed by Lu et al. (2022): https://arxiv.org/abs/2211.05228
+#  Github Repo: https://github.com/jindongwang/transferlearning/tree/master/code/deep/fixed
