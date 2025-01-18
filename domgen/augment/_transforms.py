fdf3f00fe0dec59cffa8fa3d1e49f633df5be1bc
erik-workstation <voigterik@outlook.de>
2025-01-18 20:51:44 +0100

    add custom pacs augments

--- domgen/augment/_transforms.py
+++ domgen/augment/_transforms.py
@@ -126,3 +126,71 @@ shared_aug = {
                        crop_border=False, mask_interpolation=0, fill=0, fill_mask=0, p=0.5),
     "transpose": A.Transpose(p=0.5)
 }
+
+"""Custom Augmentations for the PACS dataset"""
+
+color_geometric_aug = {
+    "color_jitter": A.ColorJitter(0.2, 0.2, 0.2, 0.1, p=1),
+    "rotate": A.Rotate((-45, 45), interpolation=1, border_mode=4, p=0.5),
+    "horizontal_flip": A.HorizontalFlip(p=0.5)
+}
+
+color_distortion_aug = {
+    "hue_saturation_value": A.HueSaturationValue((-20, 20), (-30, 30), (-20, 20), p=1),
+    "grid_distortion": A.GridDistortion(5, (-0.3, 0.3), p=0.5),
+    "transpose": A.Transpose(p=0.5)
+}
+
+contrast_geometric_aug = {
+    "clahe": A.CLAHE((1, 4), (8, 8), always_apply=False, p=1),
+    "random_resized_crop": A.RandomResizedCrop((512, 320), scale=(0.08, 1), ratio=(0.75, 1.33), p=1),
+    "rotate": A.Rotate((-90, 90), interpolation=1, border_mode=4, p=0.5)
+}
+
+noise_geometric_aug = {
+    "gauss_noise": A.GaussNoise(std_range=(0.1, 0.2), p=1),
+    "rotate": A.Rotate((-45, 45), interpolation=1, border_mode=4, p=0.5),
+    "horizontal_flip": A.HorizontalFlip(p=0.5)
+}
+
+noise_color_geometric_aug = {
+    "defocus": A.Defocus(radius=(4, 8), alias_blur=(0.2, 0.4), p=1),
+    "color_jitter": A.ColorJitter(0.2, 0.2, 0.2, 0.1, p=1),
+    "horizontal_flip": A.HorizontalFlip(p=0.5)
+}
+
+masking_color_aug = {
+    "solarize": A.Solarize(threshold=0.5, p=1),
+    "grid_dropout": A.GridDropout(ratio=0.3, unit_size_min=10, unit_size_max=20, random_offset=True, p=1.0),
+    "hue_saturation_value": A.HueSaturationValue((-20, 20), (-30, 30), (-20, 20), p=1)
+}
+
+masking_noise_aug = {
+    "xy_masking": A.XYMasking((1, 3), (1, 3), (10, 20), (10, 20), fill=0, fill_mask=0, p=1),
+    "gauss_noise": A.GaussNoise(std_range=(0.1, 0.3), p=1),
+    "rotate": A.Rotate((-30, 30), p=0.5)
+}
+
+distortion_noise_aug = {
+    "grid_distortion": A.GridDistortion(num_steps=5, distort_limit=(-0.4, 0.4), p=1),
+    "defocus": A.Defocus(radius=(3, 6), alias_blur=(0.1, 0.3), p=1),
+    "transpose": A.Transpose(p=0.5)
+}
+
+color_mask_geometric_aug = {
+    "solarize": A.Solarize(threshold=0.5, p=1),
+    "xy_masking": A.XYMasking((1, 2), (1, 3), (15, 25), (15, 25), fill=0, fill_mask=0, p=1),
+    "rotate": A.Rotate((-90, 90), p=0.5)
+}
+
+PACS_CUSTOM = {
+    "color_geometric": color_geometric_aug,
+    "color_distortion": color_distortion_aug,
+    "contrast_geometric": contrast_geometric_aug,
+    "noise_geometric": noise_geometric_aug,
+    "noise_color_geometric": noise_color_geometric_aug,
+    "masking_color": masking_color_aug,
+    "masking_noise": masking_noise_aug,
+    "distortion_noise": distortion_noise_aug,
+    "color_mask_geometric": color_mask_geometric_aug,
+}
